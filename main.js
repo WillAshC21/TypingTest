7e145ec28d5bc26e8f6a37248d9ae02aafd682d2
Ashton Williams <willashc21@gmail.com>
2020-11-21 23:34:43 -0500

    Start Screen bug fixes and finished alpha

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 // Typing text generated
 var typingMsg = [
-    // "Nobody spoke for a minute; then Meg said in an altered tone, You know the reason Mother proposed not having any presents this Christmas was because it is going to be a hard winter for everyone;",
-    // "Mr. Bennet was so odd a mixture of quick parts, sarcastic humour, reserve, and caprice.",
-    // "William Shakespeare was the son of John Shakespeare, an alderman and a successful glover (glove-maker) originally from Snitterfield, and Mary Arden.",
-    // "PHP is a general-purpose scripting language especially suited to web development.",
-    // "HTML can embed programs written in a scripting language such as JavaScript, which affects the behavior and content of web pages.",
-    // "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    // "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    // "Facilisi morbi tempus iaculis urna id volutpat lacus laoreet.",
-    "She did not hear the story as many women have heard the same, with a paralyzed inability to accept its significance.",
-    "Suddenly she came upon a little three-legged table, all made of solid glass;",
-    "This is a test screen."
+    // "Nobody spoke for a minute; then Meg said in an altered tone, You know the reason Mother proposed not having any presents this Christmas was because it is going to be a hard winter for everyone ;",
+    // "Mr. Bennet was so odd a mixture of quick parts, sarcastic humour, reserve, and caprice .",
+    // "William Shakespeare was the son of John Shakespeare, an alderman and a successful glover (glove-maker) originally from Snitterfield, and Mary Arden .",
+    // "PHP is a general-purpose scripting language especially suited to web development .",
+    // "HTML can embed programs written in a scripting language such as JavaScript, which affects the behavior and content of web pages .",
+    // "Lorem Ipsum is simply dummy text of the printing and typesetting industry .",
+    // "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua .",
+    // "Facilisi morbi tempus iaculis urna id volutpat lacus laoreet .",
+    "She did not hear the story as many women have heard the same, with a paralyzed inability to accept its significance .",
+    "Suddenly she came upon a little three-legged table, all made of solid glass ;",
+    "This is a test screen ."
 ];
+document.getElementsByClassName('typing-body')[0].style.display = "none";
+document.getElementById("welcome").innerHTML = "Welcome";
+document.getElementById("select-screen").addEventListener("click", function(){
+    document.getElementsByClassName("typing-body")[0].style.display = "block";
+    document.getElementById("title-screen").style.display = "none";
+    document.getElementById("body-box").style.background = "#ffffff" + " center fixed";
+    document.getElementById("body-box").style.backgroundSize = "cover";
+})
+
 
 // Random number generated for the text generator
 var typechoice = Math.floor((Math.random() * 10) + 0);
@@ -76,6 +85,7 @@ function home() {
     // Counts the words per minute
     if (document.getElementById('typing-value').value.charAt(wpm)) {
         document.getElementById('typing-space').innerText = "Word Count: " + wpm;
+        document.getElementById('typing-spaceover').innerText = "Word Count: " + wpm;
     }
     // Checks the current character in the text
         var txtcheck = typingMsg[typechoice].charAt(gen - 1);
@@ -95,9 +105,11 @@ function home() {
     var remain = (wpm / (60 - second) * 100);
     if (cpm ===  typingMsg[typechoice].length) {
         stop();
-        document.getElementById('typing-anon').innerHTML = "WPM: " +  Math.round(remain);
+        document.getElementById('typing-wpmover').innerHTML = "WPM: " +  Math.round(remain);
+        var total =  (Math.round(remain) * wpm) - numcorr;
+        document.getElementById('typing-totalover').innerHTML = "Score: " +  total;
     }
-    console.log(cpm + " " + typingMsg[typechoice].length);
+    console.log(wpm);
 
 }
 var gen = 0;
@@ -132,7 +144,23 @@ function stop() {
     clearInterval(inter);
     document.getElementById('typing-value').disabled  = true;
     time.innerHTML += "\t" +"Error(s): " + error;
+    modal.style.display = "block";
+    document.getElementById("typing-errorover").innerHTML = "Error(s): " + error;
+    document.getElementById("welcome").innerHTML = "Game Over";
 }
-
 // The Counter
 var counter = 0;
+
+var modal = document.getElementById('score-modal');
+
+var span = document.getElementsByClassName('close')[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
